f8ab581434ffb01d37cd433142eefe2789525d0e
Viviana Gonzalez <vivigz06@gmail.com>
2025-07-09 10:49:51 -0300

    Servidor corriendo

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import sequelize from './src/config/database.js';
+// import characterRoutes from './routes/characterRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// app.use('/api/characters', characterRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  });
+});
